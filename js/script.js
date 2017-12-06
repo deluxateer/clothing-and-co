e2887a67e494b4ca13f0b3eb2d1ae1c93ceff572
Deluxateer <deluxateer@gmail.com>
2017-12-05 20:49:51 -0500

    update arrivals content, navbar, shop-men-women, desktop view of nav

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// const closeBtn = document.querySelector('.close-btn');
+
 const menuIcon = document.querySelector('#menu-icon');
 const mainPage = document.querySelector('#site-wrap');
 const navBar = document.querySelector('#navBar');
@@ -6,39 +6,49 @@ const overLay = document.querySelector('.overlay');
 let isOpen = false;
 
 function openNav() {
-  event.stopPropagation();
-  navBar.style.width = "250px";
-  // navBar.style.paddingTop = document.querySelector('#mobile-nav').offsetHeight + "px";
-  mainPage.style.left = "250px";
-  menuIcon.classList.toggle('change');
-  // Activate Overlay
-  overLay.style.display = "block";
-  // overLay.style.opacity = "1";
-  overLay.style.backgroundColor = "rgba(0,0,0,.45)"
-  overLay.style.left = navBar.style.width;
-  isOpen = true;
+    event.stopPropagation();
+    navBar.style.width = "250px";
+    mainPage.style.left = "250px";
+    menuIcon.classList.toggle('change');
+
+    // Activate Overlay
+    overLay.style.display = "block";
+    overLay.style.backgroundColor = "rgba(0,0,0,.45)"
+    overLay.style.left = navBar.style.width;
+    isOpen = true;
 }
 
 function closeNav() {
-  navBar.style.width = "0";
-  mainPage.style.left = "0";
-  menuIcon.classList.toggle('change');
-  // Deactivate Overlay
-  overLay.style.display = "none";
-  overLay.style.backgroundColor = "transparent";
-  // overLay.style.opacity = "0";
-  isOpen = false;
+    navBar.style.width = "0";
+    mainPage.style.left = "0";
+    menuIcon.classList.toggle('change');
+
+    // Deactivate Overlay
+    overLay.style.display = "none";
+    overLay.style.backgroundColor = "transparent";
+    isOpen = false;
+
+  if (window.innerWidth >= 992) {navBar.style.width = "100%";}
 }
 
-// closeBtn.addEventListener('click', closeNav);
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth >= 992) {
+    document.querySelector('#mobile-nav').appendChild(navBar);
+  }
+});
 menuIcon.addEventListener('click', () => {
-  if (!isOpen) {openNav();}
-  else         {closeNav();}
+  if (!isOpen) { openNav(); }
+  else         { closeNav(); }
 });
 mainPage.addEventListener('click', () => {
-  if (isOpen) {closeNav();}
+  if (isOpen) { closeNav(); }
 });
-
-// const bannerWidth = document.querySelector('.banner-left-side');
-// const bannerContainer = document.querySelector('#alert-banner .container');
-// bannerContainer.style.width = bannerWidth.style.width;
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 992) {
+    closeNav();
+  } else if (window.innerWidth <= 991 && !isOpen) {
+    navBar.style.width = "0";
+    menuIcon.classList.remove('change');
+  }
+  document.querySelector('#mobile-nav').appendChild(navBar);
+});
